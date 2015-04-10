bdebfef55456f977f7d1115c5b9ad52e7e824330
Mitchell Bray <mitchellbray@CPE-58-166-66-22.lnse5.cht.bigpond.net.au>
2015-04-10 23:05:11 +1000

    fixed error

--- _admin/functions.php
+++ _admin/functions.php
@@ -8,7 +8,7 @@ add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 
 /* admin check */
 if ( current_user_can( 'manage_options' ) ) {
-add_action( 'widgets_init', 'widgets_init' );
+
 } else {
     /* A user without admin privileges */
 }
